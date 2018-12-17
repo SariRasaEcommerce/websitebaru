a68283392ea582894b9553dfa4916a05ab7fd9f0
ARDHAN\Ardhan <febriansyahardhan@gmail.com>
2018-12-17 07:01:48 +0700

    contact us

---
+++ CI/application/controllers/Contact_us.php
@@ -0,0 +1,11 @@
+<?php
+
+class Contact_us extends CI_Controller {
+
+public function index()
+	{
+		$this -> load -> view('Contact_us/V_contactus');
+	}
+}
+
+?>
---
+++ CI/application/views/Contact_us/V_contactus.php
@@ -0,0 +1,21 @@
+<?php $this->load->view('Homepage/Javascript_footer'); ?>
+<?php $this->load->view('Homepage/Top_bar'); ?>
+<?php $this->load->view('Homepage/Navbar_mainmenu'); ?>
+<?php $this->load->view('Login/Header_text'); ?>
+<h2><span>Sari Rasa Pusat Oleh-oleh Khas Jember</span></h2>
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<hr>
+<h3>Hubungi Kami</h3>
+<h3>Telepon : 0812-3484-9077 (Rahmadi)</h3>
+<h3>Facebook : <a href="http://facebook.com/sarirasa.oleholehjember">Sari Rasa</h3>
+
+</body>
+</html>
+
+
+							<?php $this->load->view('Homepage/Footer'); ?>
+							<?php $this->load->view('Homepage/Copyright'); ?>
